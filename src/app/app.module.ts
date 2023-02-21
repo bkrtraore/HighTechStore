826092c7ed4c3ef781643994a69719448137afff
Bakari Traoré <41011192@parisnanterre.fr>
2023-02-21 11:47:20 +0100

    for thomas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { PcPortableComponent } from './pc-portable/pc-portable.component';
+import { PcBureauComponent } from './pc-bureau/pc-bureau.component';
+import { PcAccessoiresComponent } from './pc-accessoires/pc-accessoires.component';
+import { ConnexionComponent } from './connexion/connexion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    PcPortableComponent,
+    PcBureauComponent,
+    PcAccessoiresComponent,
+    ConnexionComponent
   ],
   imports: [
     BrowserModule,
